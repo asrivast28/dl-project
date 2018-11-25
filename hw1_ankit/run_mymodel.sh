63b96cc1b0790eff8de86fbeefc9510a289c96cc
Ankit Srivastava <asrivast28@gmail.com>
2018-11-25 16:05:15 -0500

    Adding a modified version of my HW1 code.

---
+++ hw1_ankit/run_mymodel.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#############################################################################
+# TODO: Modify the hyperparameters such as hidden layer dimensionality, 
+#       number of epochs, weigh decay factor, momentum, batch size, learning 
+#       rate mentioned here to achieve good performance
+#############################################################################
+python -u train.py \
+    --model mymodel \
+    --kernel-size 3 \
+    --hidden-dim 32 \
+    --epochs 20 \
+    --weight-decay 0.01 \
+    --momentum 0.9 \
+    --batch-size 200 \
+    --lr 0.000278 | tee mymodel.log
+#############################################################################
+#                             END OF YOUR CODE                              #
+#############################################################################
